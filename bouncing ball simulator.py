fcfc9c245f428fb0b49ebbcf8d4deb3235873ae9
Fabio O'Ryan-Paulo <97351279+FabsOP@users.noreply.github.com>
2024-06-30 12:42:37 +0200

    Update bouncing ball simulator.py

--- bouncing ball simulator.py
+++ bouncing ball simulator.py
@@ -1,16 +1,3 @@
-######################## READ ME #########################################
-# Hi I'm Fabio :p , a computer science and physics double major student at the University of Cape Town.
-# I was bored one day so thought it would be cool to combine those skills and make a little simulation :3
-# Enjoy !
-#- November 2023
- 
-# A Physics Bouncing ball simulation using the vpython python module
-# How to use:
-# Click anywhere on screen to spawn a ball. 
-# feel free to modify the gravitational acceleration (g), damping factor , and time steps (dt) and monitor the effects
-
-#More simulations coming soon. Stay tuned !
-
 ###################### Imports #######################################
 from vpython import *
 
